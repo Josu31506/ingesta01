2c062f30698721e6d90da8c6ae61786bd0c078ae
Josu31506 <josuehernandezyataco@gmail.com>
2025-09-24 07:36:22 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -4,9 +4,10 @@ ficheroUpload = "data.csv"
 nombreBucket = "hjosue-storage-s2"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, "ingesta/" + ficheroUpload)
+response = s3.upload_file(ficheroUpload, nombreBucket, "ingesta/" + ficheroUpload)
 print(response)
 
 
 print("Ingesta completada")
+
 
